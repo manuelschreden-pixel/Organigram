94060d0ba50a8f83f6682172b77f80ab0e147ad5
eduseiman001 <eduseiman001@fh-kaernten.at>
2026-01-26 17:56:52 +0100

    Added Route for routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return redirect()->route('admin');
-//});
+Route::get('/', function () {
+    return redirect('/admin');
+});
